ce1fb47afd518859f8fbbeed06cb09c5640f3eb3
Robee <robert.filic@gmail.com>
2018-05-10 09:42:29 +0300

    Elaborated error structure

--- src/models/users/errors.py
+++ src/models/users/errors.py
@@ -1,9 +1,17 @@
-
-
-class UserNotExistsError(Exception):
+class UserError(Exception):
     def __init__(self, message):
         self.message = message
 
-class IncorrectPasswordError(Exception):
-    def __init__(self, message):
-        self.message = message
+class UserNotExistsError(UserError):
+    pass
+
+
+class IncorrectPasswordError(UserError):
+    pass
+
+
+class UserAlreadyRegisteredError(UserError):
+    pass
+
+class InvalidEmailError(UserError):
+    pass
